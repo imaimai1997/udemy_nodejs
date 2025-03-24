251cf2c1212ed0c8b3727c83634f2b12b1da8144
imaimai <onakasuitanaapple@gmail.com>
2025-03-24 20:46:32 +0900

    study 24

--- 02_node_basic/030_write_file/start/example.js
+++ 02_node_basic/030_write_file/start/example.js
@@ -1,1 +1,9 @@
-console.log('hello, node.js');
+const fs = require('fs');
+// console.log(__dirname);
+
+fs.writeFileSync(__dirname+'/test.txt','hello,node.js');
+
+
+
+
+// console.log('hello, node.js');
