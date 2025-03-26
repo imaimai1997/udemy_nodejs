2eedb1012ea3962a1cf19c80819a0cc3b0bba100
imaimai <onakasuitanaapple@gmail.com>
2025-03-26 20:56:28 +0900

    study 36

--- 03_npm/010_intro/start/main.js
+++ 03_npm/010_intro/start/main.js
@@ -1,8 +1,10 @@
-import _ from "lodash";
+const cloneDeep = require("lodash/cloneDeep") ;
+
+// import _ from "lodash";
 
 const original = { prop: { nested: "value" } };
 // オブジェクトの複製
-const cloned = _.cloneDeep(original);
+const cloned = cloneDeep(original);
 cloned.prop.nested = "new value";
 
 console.log(original,cloned);
