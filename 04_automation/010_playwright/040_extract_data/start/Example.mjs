bbba92b0c2ca963a52b0d7778f3f619056444f06
imaimai <onakasuitanaapple@gmail.com>
2025-03-27 20:37:41 +0900

    study 49

--- 04_automation/010_playwright/040_extract_data/start/Example.mjs
+++ 04_automation/010_playwright/040_extract_data/start/Example.mjs
@@ -6,7 +6,25 @@ import { chromium } from "@playwright/test";
   await page.goto("http://localhost:3000");
   const htmlStr = await page.content();
 
-  console.log(htmlStr);
+  // console.log(htmlStr);
 
+
+  // cssセレクター
+  const pageTitleLocator = await page.locator('.navbar-brand');
+  const pageTitle = await pageTitleLocator.innerText();
+  // console.log(pageTitle);
+  
+  // 文字列で要素を取得
+  const textLocator = await page.locator("text=名刺管理アプリ");
+  const pageText = await textLocator.innerText();
+  // console.log(pageText);
+  
+
+  // xpathで要素を取得
+  const xpathLocator = await page.locator("xpath=//*[@id=\"__next\"]/nav/div/a");
+  const xpathText = await xpathLocator.innerText();
+  console.log(xpathText);
+ 
   await browser.close();
 })();
+
