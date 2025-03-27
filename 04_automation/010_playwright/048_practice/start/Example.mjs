1b9ee867bb89f55a7fd9d975e1c269bac72f0b29
imaimai <onakasuitanaapple@gmail.com>
2025-03-28 08:51:40 +0900

    study 53

--- 04_automation/010_playwright/048_practice/start/Example.mjs
+++ 04_automation/010_playwright/048_practice/start/Example.mjs
@@ -9,6 +9,15 @@ import { chromium } from "@playwright/test";
   const page = await browser.newPage();
   await page.goto("http://localhost:3000");
 
+  const inputLocator = page.locator("//*[@id=\"__next\"]/div/div[1]/label/input");
+  await page.waitForTimeout(2000);
+  await inputLocator.type('佐藤');
+
+  const nameLocator = await page.locator(".cards.list-group-item >> nth=-1");
+  const searchName = await nameLocator.innerText();
+
+  console.log(searchName)
+
   await browser.close();
 
 })();
