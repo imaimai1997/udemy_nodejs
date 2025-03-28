02aa948c81e2c9a6c2f9532acf1babf9afa7e898
imaimai <onakasuitanaapple@gmail.com>
2025-03-28 19:56:53 +0900

    study 57

--- 04_automation/010_playwright/080_practice_error_handling/start/Example.mjs
+++ 04_automation/010_playwright/080_practice_error_handling/start/Example.mjs
@@ -1,4 +1,8 @@
 import { chromium } from "@playwright/test";
+import * as fs from "fs";
+import { Parser } from "json2csv";
+import env from "dotenv";
+env.config();
 
 /**
  * 練習問題
@@ -12,6 +16,51 @@ import { chromium } from "@playwright/test";
   const browser = await chromium.launch();
   const page = await browser.newPage();
   await page.goto(process.env.TARGET_URL);
+  // await page.goto("http://localhost:3000");
+
+  const pageLocator = page.locator(".page-link.page-number >> nth=2")
+  await pageLocator.click();
+
+  const cardLocators = page.locator(".cards.list-group-item");
+  const cardCount = await cardLocators.count();
+
+  const fetchedCards = [];
+  for(let i = 0; i < cardCount; i++) {
+    const cardLocator = cardLocators.locator(`nth=${i} >> a`);
+    const cardText = await cardLocator.textContent();
+
+    await cardLocator.click();
+
+    const companyClassLocator = page.locator('.division')
+    const companyClassText = await companyClassLocator.textContent();
+
+    if (companyClassText.includes('係長')){
+      let companyText = "";
+      try{
+    const companyLocator = page.locator('.card-title.company');
+    companyText = await companyLocator.textContent();
+    }catch {}
+
+    fetchedCards.push({
+      company: companyText,
+      name: cardText
+    });
+    
+  }
+
+    const backLocator = page.locator('text=戻る');
+    await backLocator.click();
+
+  }
 
   await browser.close();
+
+  const parser = new Parser();
+  const csv = parser.parse(fetchedCards);
+  
+  fs.writeFileSync("./text-data.csv", csv);
+
+
+
+ 
 })();
