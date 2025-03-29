5056e652def45fe365c9f46ec17bbe8951b8fc51
imaimai <onakasuitanaapple@gmail.com>
2025-03-29 14:55:36 +0900

    study 69

--- 04_automation/030_cron/010_scheduling/start/Example.mjs
+++ 04_automation/030_cron/010_scheduling/start/Example.mjs
@@ -1,7 +1,6 @@
 import cron from "node-cron";
+import { addEmployeesToGS } from "./google-sheet.mjs"; 
 
-cron.schedule('* * * * * *', () => console.log('毎秒に実行'));
-cron.schedule('*/3 * * * * *', () => console.log('3秒ごとに実行'));
-cron.schedule('* * * * *', () => console.log('毎分に実行'));
-cron.schedule('0 0 9,18 * * *', () => console.log('毎日9時,18時に実行'));
-cron.schedule('30 30 12 * * *', () => console.log('毎日12時30分30秒に実行'));   
+cron.schedule("54 14 * * *", () =>{
+  addEmployeesToGS();
+});
