22977915b64192a20de224b70830c58791967faf
imaimai <onakasuitanaapple@gmail.com>
2025-03-30 12:45:33 +0900

    study 90

--- 06_backend/030_browser_and_server/start/app.mjs
+++ 06_backend/030_browser_and_server/start/app.mjs
@@ -3,13 +3,16 @@ import * as http from "http";
 // http://localhost:8080/hello -> hello
 // http://localhost:8080/bye -> bye
 
-const server = http.createServer(function(req, res) {
-    console.log(req.url);
-    if(req.url === '/hello') {
-        res.end('hello');
-    } else if(req.url === '/bye') {
-        res.end('bye');
-    }
+//scriptタグで囲まれたものはブラウザ側で実行される。
+//そのほかはnode.js上（サーバー側）で実行される
+
+const server = http.createServer(function (req, res) {
+  console.log(req.url);
+  if (req.url === "/hello") {
+    res.end('<script>window.alert("frontend")</script>');
+  } else if (req.url === "/bye") {
+    res.end("bye");
+  }
 });
 
 server.listen(8080);
