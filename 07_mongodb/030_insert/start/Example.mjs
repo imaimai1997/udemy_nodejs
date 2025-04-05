8663ba083bd5e871811e07e3e003eea02a513756
imaimai <onakasuitanaapple@gmail.com>
2025-04-05 20:38:34 +0900

    study 115

--- 07_mongodb/030_insert/start/Example.mjs
+++ 07_mongodb/030_insert/start/Example.mjs
@@ -1,14 +1,18 @@
-import env from 'dotenv';
+import env from "dotenv";
 env.config();
 
-import { MongoClient, ServerApiVersion, ObjectId } from 'mongodb';
-const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
+const client = new MongoClient(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function getCollection() {
   try {
     await client.connect();
-    const db = client.db('bookshelf');
-    return db.collection('books');
+    const db = client.db("bookshelf");
+    return db.collection("books");
   } catch {
     await client.close();
   }
@@ -17,6 +21,13 @@ async function getCollection() {
 insertBook();
 async function insertBook() {
   const col = await getCollection();
+  const result = await col.insertOne({
+    title: "こんにちは3",
+    int: 10,
+    dt: new Date(),
+    array: [0, 1, 2],
+  });
+  console.log(result);
 
   await client.close();
 }
