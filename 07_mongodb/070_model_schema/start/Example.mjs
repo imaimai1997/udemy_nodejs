0ce592f0c25832e25df0ff843c4bff7582fd2c32
imaimai <onakasuitanaapple@gmail.com>
2025-04-05 21:31:07 +0900

    study 120

--- 07_mongodb/070_model_schema/start/Example.mjs
+++ 07_mongodb/070_model_schema/start/Example.mjs
@@ -2,17 +2,27 @@ import { connect, Schema, model, Mixed } from "mongoose";
 import env from "dotenv";
 env.config();
 
-mongoose.connect(process.env.MONGO_URI);
-const catShema = Schema({
-  name: String,
-  size: Number,
-  bool: Boolean,
-  dt: Date,
+connect(process.env.MONGO_URI);
+const catSchema = Schema({
+  name: { type: String, required: true },
+  size: { type: Number, required: true },
+  bool: { type: Boolean, default: false, alias: "b" },
+  dt: {
+    type: Date,
+    set: function (newVal) {
+      return new Date(newVal);
+    },
+    get: function (val) {
+      return val instanceof Date ? val : new Date(val);
+    },
+  },
   array: [],
   anything: Mixed,
 });
-const Cat = model("Cat", catShema);
+const Cat = model("Cat", catSchema);
 
 const kitty = new Cat();
 kitty.name = "Zildjian";
+kitty.size = 1;
+kitty.dt = "2025/4/5";
 kitty.save().then(() => console.log("meow"));
