1633aa803cb309662c3d6ec7631bbeacb074d8b3
imaimai <onakasuitanaapple@gmail.com>
2025-04-05 14:27:30 +0900

    study 105

--- 06_backend/110_rest_api/start/app.mjs
+++ 06_backend/110_rest_api/start/app.mjs
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from "express";
 
 const PORT = 8080;
 const app = express();
@@ -11,10 +11,35 @@ const products = [
   { name: "clock", price: 700 },
 ];
 
-app.get('/products', function (req, res) {
+app.get("/products", function (req, res) {
   res.json(products);
 });
 
+app.post("/create-product", function (req, res) {
+  const newProduct = req.body;
+  products.push(newProduct);
+  console.log(products);
+  res.json(newProduct);
+});
+
+app.post("/delete-product", function (req, res) {
+  const deleteId = req.body.id;
+  products.splice(deleteId, 1);
+  console.log(products);
+  res.json({ deleteId });
+});
+
+app.post("/update-product", function (req, res) {
+  const targetProduct = products[req.body.id];
+  if (req.body.hasOwnProperty("price")) {
+    targetProduct.price = req.body.price;
+  }
+  if (req.body.hasOwnProperty("name")) {
+    targetProduct.name = req.body.name;
+  }
+  console.log(products);
+  res.json(targetProduct);
+});
 app.listen(PORT, function () {
   console.log(`Server start: http://localhost:${PORT}`);
 });
