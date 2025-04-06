f0b0fd845872ad6686cd68b1a284195a577314d5
imaimai <onakasuitanaapple@gmail.com>
2025-04-06 10:37:58 +0900

    study 124

--- 08_bookshelf_backend/server/api-routes/books.mjs
+++ 08_bookshelf_backend/server/api-routes/books.mjs
@@ -1,34 +1,43 @@
-import express from 'express';
-import { body } from 'express-validator';
+import express from "express";
+import { body } from "express-validator";
 import { requestErrorHandler } from "../helpers/helper.mjs";
-import { getBookById, getAllBooks, deleteBook, registBook, updateBook } from '../controllers/books.mjs';
+import {
+  getBookById,
+  getAllBooks,
+  deleteBook,
+  registBook,
+  updateBook,
+} from "../controllers/books.mjs";
 
 const router = express.Router();
 
-router.get('/', requestErrorHandler(getAllBooks));
+// router.get('/', requestErrorHandler(getAllBooks));
+router.get("/", function (req, res) {
+  res.send("./api/books");
+});
 
-router.get('/:id', requestErrorHandler(getBookById));
+router.get("/:id", requestErrorHandler(getBookById));
 
 router.post(
-    '/', 
-    body('title').notEmpty(), 
-    body('description').notEmpty(), 
-    body('comment').notEmpty(), 
-    body('rating').notEmpty().isInt({ min: 1, max: 5 }), 
-    requestErrorHandler(registBook)
+  "/",
+  body("title").notEmpty(),
+  body("description").notEmpty(),
+  body("comment").notEmpty(),
+  body("rating").notEmpty().isInt({ min: 1, max: 5 }),
+  requestErrorHandler(registBook)
 );
 
 // validator.js
 // https://github.com/validatorjs/validator.js
 router.patch(
-    '/:id', 
-    body('title').optional().notEmpty(), 
-    body('description').optional().notEmpty(), 
-    body('comment').optional().notEmpty(), 
-    body('rating').optional().notEmpty().isInt({ min: 1,max: 5 }), 
-    requestErrorHandler(updateBook)
+  "/:id",
+  body("title").optional().notEmpty(),
+  body("description").optional().notEmpty(),
+  body("comment").optional().notEmpty(),
+  body("rating").optional().notEmpty().isInt({ min: 1, max: 5 }),
+  requestErrorHandler(updateBook)
 );
 
-router.delete('/:id', requestErrorHandler(deleteBook));
+router.delete("/:id", requestErrorHandler(deleteBook));
 
 export default router;
